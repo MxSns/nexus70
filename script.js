1e6bdb2a725ae6b7980a0c881d98eb2c789438f0
Oz <oz@pop-os.localdomain>
2023-12-20 02:59:51 +0100

    reduce pictures weight

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-$(function(){
-    
-    $('.intro-logo').animate({opacity: 1}, 1000, function() {
-        $('.decouvrir').animate({opacity: 1}, 1200);
+document.addEventListener('DOMContentLoaded', function () {
+    var introLogo = document.querySelector('.intro-logo');
+    var decouvrir = document.querySelector('.decouvrir');
+
+    fadeIn(introLogo, 1000, function () {
+        fadeIn(decouvrir, 1200);
     });
-                        
 });
+
+function fadeIn(element, duration, callback) {
+    var opacity = 0;
+    var interval = 20;
+    var delta = interval / duration;
+
+    function increaseOpacity() {
+        opacity += delta;
+        element.style.opacity = opacity;
+
+        if (opacity >= 1) {
+            clearInterval(timer);
+            if (callback) callback();
+        }
+    }
+
+    var timer = setInterval(increaseOpacity, interval);
+}
 
 
 /* function for returning a message at the form submission */
